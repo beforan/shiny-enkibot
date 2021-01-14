548d343b3373a5faa5a4de1c8b6b5a408d44c967
Jon Couldridge <jcouldridge@outlook.com>
2021-01-14 16:01:06 +0000

    ⚡ initial display of new structure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Button, Code, Stack } from "@chakra-ui/react";
+import { Box, Button, Code, Heading, Stack } from "@chakra-ui/react";
 import Head from "next/head";
 import { useState } from "react";
 import { getEnkibotJson } from "../lib/enki-data";
@@ -19,7 +19,7 @@ const HeadTag = () => (
 );
 
 const Home = ({ enkiData }) => {
-  const [showData, setShowData] = useState(true);
+  const [showData, setShowData] = useState(false);
   return (
     <>
       <HeadTag />
@@ -31,9 +31,43 @@ const Home = ({ enkiData }) => {
           {showData ? "Hide" : "Show"} Data
         </Button>
         {showData && <pre>{JSON.stringify(enkiData, null, 2)}</pre>}
+
+        {Object.keys(enkiData).map((section) => (
+          // TODO: filter sections? Anchor them?
+          <Section title={section} groups={enkiData[section]} />
+        ))}
       </Stack>
     </>
   );
 };
+
+const Section = ({ title, groups }) => {
+  // TODO: jobs filtering
+  return (
+    <>
+      <Heading>{title}</Heading>
+      {groups.map((group) => (
+        <TipsGroup {...group} />
+      ))}
+    </>
+  );
+};
+
+const TipsGroup = ({ jobs, tips }) => (
+  <>
+    {jobs && (
+      <Heading size="sm">
+        {jobs.map((combo) => (
+          <JobsIcon jobs={combo} />
+        ))}
+      </Heading>
+    )}
+    {tips.map((tip) => (
+      <div>{tip}</div>
+    ))}
+  </>
+);
+
+const JobsIcon = ({ jobs }) => <div>{jobs.join("+")}</div>;
 
 export default Home;
